b5f07ea6b35c275f94f4ef4532eedcd777188f7a
gabrieltacosta <gabriel.tuka@gmail.com>
2024-04-16 17:35:40 -0300

    Second cCommit

--- setup/settings.py
+++ setup/settings.py
@@ -1,3 +1,4 @@
+from django.contrib.messages import constants
 from pathlib import Path
 import os
 
@@ -105,9 +106,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
+STATIC_ROOT = os.path.join('static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Messages
+
+MESSAGE_TAGS = {
+    constants.DEBUG: 'alert-primary',
+    constants.ERROR: 'alert-danger',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info',
+    constants.WARNING: 'alert-warning',
+}
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
 
 
 # Create your views here.
 def cadastro(request):
     if request.method == 'GET':
-        return render(request, 'usuarios/cadastro.html')
+        return render(request, 'cadastro.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if senha != confirmar_senha:
+            messages.add_message(request, constants.ERROR,
+                                 "A senha e confirmar senha devem ser iguais")
+            return redirect('/usuarios/cadastro')
+
+        if len(senha) < 6:
+            messages.add_message(request, constants.ERROR,
+                                 "A senha deve ter no mínimo 6 dígitos")
+            return redirect('/usuarios/cadastro')
+
+        users = User.objects.filter(username=username)
+
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=senha,
+        )
+
+        if users.exists():
+            messages.add_message(request, constants.ERROR,
+                                 "Já existe um usuário com esse username")
+            return redirect('/usuarios/cadastro')
+
+        return redirect('/usuarios/login')
+
+
+def login(request):
+    pass
