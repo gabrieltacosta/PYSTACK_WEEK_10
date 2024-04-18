cafdd5bf16532c14d6a304795288917f9a9e9908
gabrieltacosta <gabriel.tuka@gmail.com>
2024-04-17 21:11:55 -0300

    Third Commit

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.messages import constants
-from django.contrib import messages
+from django.contrib import messages, auth
 
 
 # Create your views here.
@@ -41,5 +41,20 @@ def cadastro(request):
         return redirect('/usuarios/login')
 
 
-def login(request):
-    pass
+def login_view(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get("senha")
+        user = auth.authenticate(request, username=username, password=senha)
+        if user:
+            auth.login(request, user)
+            return redirect('/pacientes/home')
+        messages.add_message(request, constants.ERROR, 'Usuário ou senha incorretos')
+    return redirect('/usuarios/login')
+
+
+def sair(request):
+    auth.logout(request)
+    return redirect('/usuarios/login')
